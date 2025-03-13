f4d8c913e5465da03977e30ce9f52cb94d3a235a
nafis@aidocbuilder.com <rongdhong@54321>
2025-03-14 01:44:38 +0600

    config

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
       plugins: conditionalPlugins,
     }),
     tempo(),
+    base: process.env.VITE_BASE_PATH || "/SupplyChainHub"
   ],
   resolve: {
     preserveSymlinks: true,
